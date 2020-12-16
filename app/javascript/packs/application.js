520163c52b03cc7228d2c778b1ab8240d77dfc97
Abdulmelik SAK <melik.sak@icloud.com>
2020-12-16 10:12:04 +0100

    mise en place de tailwind

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,8 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+import '../stylesheets/application.scss';
+
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
@@ -23,7 +25,6 @@ require("channels")
 // ----------------------------------------------------
 
 // External imports
-import "bootstrap";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
